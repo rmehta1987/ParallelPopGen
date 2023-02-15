423609aad008587ae7d658438dfefea32defcfb6
Rahul Mehta <rmehta1987@gmail.com>
2023-02-15 11:38:46 -0600

    added slurm scripts for all samples

---
+++ examples/example_dadi/create_slurm_sim_11.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=popgensim
+#SBATCH --account=pi-jjberg
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+# TO USE V100 specify --constraint=v100
+# TO USE RTX600 specify --constraint=rtx6000
+#******SBATCH --constraint=v100   # constraint job runs on V100 GPU use
+#SBATCH --ntasks-per-node=1 # num cores to drive each gpu
+#SBATCH --cpus-per-task=2   # set this to the desired number of threads
+
+
+module load cuda
+bash scripts_sample_sel_data/run_lots_of_sims_1e8_part3.sh
---
+++ examples/example_dadi/testscript.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+bash scripts_sample_sel_data/run_lots_of_sims_1e4_part1.sh
