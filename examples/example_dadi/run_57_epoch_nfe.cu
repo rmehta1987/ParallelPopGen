8e4b6f5c9c028c11c334987ccdde439a64f93a6c
Rahul Mehta <rmehta1987@gmail.com>
2023-04-24 12:09:34 -0500

    fixed sample_size problem

--- examples/example_dadi/run_57_epoch_nfe.cu
+++ examples/example_dadi/run_57_epoch_nfe.cu
@@ -7,6 +7,8 @@
 #include "../../3P/go_fish.cuh"
 #include "spectrum.h"
 #include <chrono>
+#include <fstream>
+
 
 /*
 This folllows schiffels-durbin demographic model
@@ -98,13 +100,48 @@ auto run_model(float num_sites, float mu, float sel_coef, unsigned int seed1, un
 
     // Complex UK Biobank Demographic Histroy
     //  I believe it goes - generation pop size_1, generation time_1 then eneration pop size_2, generation time_2
-    auto demography_model = Sim_Model::make_piecewise_evolution_model(cp(14448), 4545, cp(200000), 8483);
+    auto demography_model = Sim_Model::make_piecewise_evolution_model(cp(14448), 4545,  cp(14068), 8483, 
+    cp(14068), 11956, 
+    cp(14464), 15063,    
+    cp(15208), 20439, 
+    cp(16256), 24984, 
+    cp(17618), 28922, 
+    cp(19347), 32395, 
+    cp(21534), 35501,
+    cp(24236), 38312,
+    cp(27367), 40877,
+    cp(30416), 43238,
+    cp(32060), 45423,
+    cp(31284), 47457,
+    cp(29404), 48424,
+    cp(26686), 49360,
+    cp(23261), 50268,
+    cp(18990), 50420,
+    cp(16490), 50784,
+    cp(12958), 51440,
+    cp(7477), 52775,
+    cp(5791), 53004,
+    cp(4670), 53431,
+    cp(3841), 53824,
+    cp(3372), 54188,
+    cp(3287), 54527,
+    cp(3359), 54688,
+    cp(3570), 54844,
+    cp(4713), 55142,
+    cp(5661), 55284,
+    cp(7540), 55423,
+    cp(11375), 55557,
+    cp(14310), 55688,
+    cp(13292), 55816,
+    cp(14522), 54361,
+    cp(613285), 55890,
+    cp(1000000), 55940); // last one is the additional 50 generations 
     
 
 	return GO_Fish::run_sim(input, mutation_rate , demography_model, migration , selection, inbreeding, dominance, Sampling::off());
 }
 
-void print_sfs(int num_iterations, int num_samples, float num_sites, float mu, float sel_coef){
+void print_sfs(int num_iterations, int num_samples, float num_sites, float mu, float sel_coef, std::string file_name){
 	
   auto num_iter = num_iterations;															//number of iterations
 	auto sample_size = num_samples;													//number of samples in SFS
@@ -115,20 +152,56 @@ void print_sfs(int num_iterations, int num_samples, float num_sites, float mu, f
     GO_Fish::allele_trajectories b;
     std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
     start = std::chrono::high_resolution_clock::now();
+    std::vector<std::vector<float>> results(num_iter,  std::vector<float> (sample_size, 0)); // storage for SFS results
+    std::vector<float> average(sample_size, 0.0);
 
+  std::cout << "Starting iteration 0 of simulation" << std::endl;
 	for(auto j = 0; j < num_iter; j++)
   {
-		b = run_model(num_sites, mu, sel_coef, 0xbeeff00d + 2*j, 0xdecafbad - 2*j);
-		Spectrum::site_frequency_spectrum(my_spectra,b,0,1,sample_size);
+		b = run_model(num_sites, mu, sel_coef, 0xbeeff00d + 2 * j+3, 0xdecafbad - 2 * j+6);
+		Spectrum::site_frequency_spectrum(my_spectra,b,0,0,sample_size);
+    for (int i = 0; i < sample_size; i++)
+        {
+            results[j][i] = my_spectra.frequency_spectrum[i];
+            // calculates average, // [ old_average * (n-1) + new_value ] / n
+            if (j == 0)
+            {
+                average[i] += my_spectra.frequency_spectrum[i]; 
+            }
+            else
+            {
+                average[i] = (average[i] * (j-1.0) + my_spectra.frequency_spectrum[i]) * 1.0/j;
+            }
+        }
+        if (j%2==0)
+        {
+             std::cout << "Finished iteration " << j << " of simulation" << std::endl;
+        }
 	}
+  std::cout << "Finished all iterations of simulation" << std::endl;
 	
 	end = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double,std::milli> elapsed_ms = end - start;
 	std::cout<<"\nnumber of mutations in simulation: " << b.maximal_num_mutations() << "\nnumber of mutations in SFS: "<< my_spectra.num_mutations <<"\ntime elapsed (ms): "<< elapsed_ms.count()/num_iter << std::endl << std::endl;
-	for(int i = 1; i < sample_size; i++)
+
+  std::ofstream output_file(file_name);
+    //std::string output_file_name_debug = "last_sim_" + file_name;
+    //std::ofstream output_file2(output_file_name_debug);
+
+    for (int i = 0; i < sample_size; i++)
+    {
+        output_file << average[i] << "\n";
+        //output_file2 << my_spectra.frequency_spectrum[i] << "\n";;
+
+    }
+	
+  /*for(int i = 1; i < sample_size; i++)
   { 
       std::cout<< my_spectra.frequency_spectrum[i]/my_spectra.num_mutations << std::endl; 
-  }
+  }*/
+
+  
+    
 }
 
 ////////////////////////////////////////////////////////////
@@ -141,7 +214,7 @@ int main(int argc, char **argv)
 
     // this is a point selection coefficient the selection coefficient will remain the same for the population, this is the un-scaled selection coefficient
     float PointSel = -.005;
-    int num_samples = 10000;
+    int num_samples = 111710;
     float mutrate = (9.6111f * pow(10.f, -9));
     std::string file_name = "out_file.txt";
 
@@ -151,8 +224,7 @@ int main(int argc, char **argv)
 
     if (argc != 3) // 3 Total parameters, [executable, unscaled selection coefficient, num_samples, file_name]
     {
-        fprintf(stderr, "Error: The number of arguments given in the command line is not correct. In this version you need to pass in a selection cofficient and unscaled mutation rate, format is: ./GOFish unscaled_selection coefficient num_samples \n");
-        // exit(8);
+        fprintf(stderr, "Warning: The number of arguments given in the command line is not correct. In this version you need to pass in a selection cofficient sample size, and output_file name, format is: ./GOFish unscaled_selection coefficient num_samples out_file_name\n");
         std::cout << "Using default values" << std::endl;
     }
     else
@@ -169,7 +241,7 @@ int main(int argc, char **argv)
 
     std::cout << "Running simulations" << std::endl;
 
-    print_sfs(2, num_samples, num_sites, mutrate, PointSel); 
+    print_sfs(2, num_samples, num_sites, mutrate, PointSel, file_name); 
      
     
 }
